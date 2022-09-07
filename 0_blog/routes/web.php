3d3f2973643e490b0f1bd6d08ebf3abe42d14e99
pavle <zikicpavle@gmail.com>
2022-09-07 12:02:57 +0200

    Dynamic routes

--- 0_blog/routes/web.php
+++ 0_blog/routes/web.php
@@ -17,6 +17,30 @@ use Illuminate\Support\Facades\Route;
 // Giving the name of the view
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
     // return ['foo' => 'bar']; // JSON
 });
+
+
+Route::get('posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if (!file_exists($path)) {
+        //dump die - dd
+        //dump die debug - ddd
+        // ddd('file does not exist');
+
+
+        // abort(404);
+
+
+        return redirect('/');
+    }
+
+    //DYNAMIC variable
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z\-_]+'); //FOR $slug validation -> whereAlpha(), whereNumber()
