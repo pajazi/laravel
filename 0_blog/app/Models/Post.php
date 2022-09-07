b0ffc48b1f51e2a7c3a14dbf1db43640e2baa727
pavle <zikicpavle@gmail.com>
2022-09-07 15:45:38 +0200

    Routes Refactoring

--- 0_blog/app/Models/Post.php
+++ 0_blog/app/Models/Post.php
@@ -27,4 +27,13 @@ class Post
     {
         return static::all()->firstWhere('slug', $slug);
     }
+
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+
+        if (!$post) throw new ModelNotFoundException();
+
+        return $post;
+    }
 }
