da3c381a85b80f5c1d5c0eceb5e7859add7fd4be
pavle <zikicpavle@gmail.com>
2022-09-10 00:59:38 +0200

    End of section 9

---
+++ 0_blog/app/Http/Controllers/SessionsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class SessionsController extends Controller
+{
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($attributes)) {
+            throw ValidationException::withMessages(['email' => 'Your provided credentials could not be verified.']);
+        }
+        return redirect('/')->with('success', 'Welcome Back!');
+
+// Other way when the validation failed:
+//        return back()
+//            ->withInput()
+//            ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+    }
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+}
